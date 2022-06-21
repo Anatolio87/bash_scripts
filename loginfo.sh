c5e68a718564c4b0b31117fbe105d5a983868be3
Anatolio87 <89261969132@mail.ru>
2022-06-21 13:14:07 +0300

    test1

--- loginfo.sh
+++ loginfo.sh
@@ -4,9 +4,10 @@
 #cat /home/toly/logmeg/log/nginx/error.log | grep -n -A8 -E "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}" | grep -A8 -iE "error" | tail -n 6
 
 #cat /home/toly/logmeg/log/nginx/error.log | grep -n -E "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}.*\[error\]" | sed 's/$/\n**********************/g' | tee ~/test.txt | uniq -u -f 50
-cat /home/toly/logmeg/log/nginx/error.log | grep -n -E "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}.*\[error\]" | \
+cat /home/toly/logmeg/nginx/error.log | grep -n -E "^[0-9]{4}\/[0-9]{2}\/[0-9]{2}.*\[error\]" | \
 tail -n 20 | cut -c -250 | uniq  -f 5 | \
-sed 's/$/\n****************************************************************************************************************************/g'
+sed 's/^/var\/log\/nginx\.err Строка: /g' | \
+sed 's/$/\n****************************************************************************************************************************/g' | tee ~/GIT/bash_scripts/file-logs/nginx_logs.txt
 
 #log erpher
 #cat /home/toly/logmeg/log/megaplan/admin-exec.log | grep -A8 -E "^\[[0-9]{2}\.[0-9]{2}\.[0-9]{4}\ [0-9]{1,2}:" | grep -A8 -iE "error" 
