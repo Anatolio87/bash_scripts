b2577f01e03218dc124233db53c657320430acc6
Anatolio87 <89261969132@mail.ru>
2022-06-28 15:11:14 +0300

    test11

---
+++ key.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#http://kb.megoplan.ru/pages/viewpage.action?pageId=33983382
+echo "Для директора"
+user=$(sudo -u postgres psql -Atc "select i.child_id from field_value.ii_user_inheritance i, sdf.perm_user u, sdf.perm_user u2 where u.user_id=i.parent_id and i.child_id=u2.user_id and u2.can_login and u.is_group='t' and u.name='directors' limit 1" megaplan 2>/dev/null) && result=$(sudo php /var/www/megaplan/current/all/cli/run_cli.php "cli://localhost/SdfPerm/User/createOneTimeKeyAuthUser.easy?user_id=$user") && host=$(cat /var/www/megaplan/common/config/settings.ini | grep 'http.host *=' | sed 's/[="]//g;s/http.host//;s/ //g') && echo -e "\nhttp://$host/login/?one_time_key=$result\n"
+
+#Если нужен конкретный сотрудник, тогда необходимо запустить следующую часть скрипта:
+
+echo "Список сотрудников"
+sudo -u postgres psql -c "select user_id, name from sdf.perm_user" megaplan
+id_user = ""
+if [ -n $1 ] then;
+	id_user = $1
+else
+	echo -n "Введите id сотрудника мегаплана: "
+	read id_user
+fi
+php /var/www/megaplan/current/all/cli/run_cli.php "cli://megaplan/SdfPerm/User/createOneTimeKeyAuthUser.easy?user_id=$id_user"
--- size.sh
+++ size.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
+echo "Информация по базе данных"
+echo "Версия postgresql"
+psql -V
+echo "Список баз данных"
+sudo -u postgres psql -l
+echo "Работающий кластер"
+pg_lsclusters
+#sudo -u postgres psql -c "SHOW SERVER_VERSION"
 echo "*****РАЗМЕР БАЗЫ******"
 sudo -u postgres psql -c "SELECT pg_size_pretty(pg_database_size('megaplan'))"
+
+echo "Информация по файлам"
 echo "*****РАЗМЕР ФАЙЛОВ uploads****"
 du -sh /var/www/megaplan/common/var/uploads
 echo "*****РАЗМЕР ФАЙЛОВ /mnt/ext_uploads/"
 du -hs /mnt/ext_uploads
+
