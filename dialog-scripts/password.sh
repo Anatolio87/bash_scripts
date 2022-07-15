79702dd02b87b1c1cfe39cf09d0f9dbb8dcdf611
Anatolio87 <89261969132@mail.ru>
2022-07-15 08:09:47 +0000

    test23

--- dialog-scripts/password.sh
+++ dialog-scripts/password.sh
@@ -1,49 +1,22 @@
 #!/bin/sh
-
-# create a file to store the password
+#
 output="psw.txt"
-# remove the password file, if already exists.
+#удалить psw.txt если не нужен
+trap "rm -f psw.txt" 0 1 2 5 15
 
-trap "rm -f psw.txt" 2 15
-dialog --title "Password" \
+dialog --title "Изменение пароля" \
 --insecure \
 --clear \
---passwordbox "Please enter password" 10 30 2> $output
+--passwordbox "Введите новый пароль" 10 30 2> $output
 reply=$?
 case $reply in
-0) echo "You have entered Password : $(cat $output)";;
-1) echo "You have pressed Cancel";;
-255) cat $data && [ -s $data ] || echo "Escape key is pressed.";;
+0) echo "Вы успешно изменили пароль";;
+1) echo "Отмена ввода";;
+255) echo "Отмена ввода";;
 esac
 
 # getting the value into a variable
 var=$(cat $output)
-
-echo $var
-
 # remove the password file
 rm psw.txt
-
-# create a file to store the password
-output="psw.txt"
-# remove the password file, if already exists.
-
-trap "rm -f psw.txt" 2 15
-dialog --title "Password" \
---insecure \
---clear \
---passwordbox "Please enter password" 10 30 2> $output
-reply=$?
-case $reply in
-0) echo "You have entered Password : $(cat $output)";;
-1) echo "You have pressed Cancel";;
-255) cat $data && [ -s $data ] || echo "Escape key is pressed.";;
-esac
-
-# getting the value into a variable
-var=$(cat $output)
-
-echo $var
-
-# remove the password file
-rm psw.txt
+echo "Ваш новый пароль: $var" 
